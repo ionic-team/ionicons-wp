8dad8c49933d0ae23ae79f49b627551a7fc7a887
Max Lynch <ihasmax@gmail.com>
2015-11-08 13:44:54 -0600

    First

---
+++ plugin.php
@@ -0,0 +1,35 @@
+<?php 
+/**
+ * Plugin Name: Ionicons (Official Icons)
+ * Plugin URI: https://github.com/driftyco/ionicons-wp
+ * Description: Official Ionicons plugin - Add over 900 open source icons to your wordpress site with a simple shortcode
+ * Version: 1.0.0
+ * Author: Ionic
+ * Author URI: http://ionic.io/
+ * License: MIT
+ */
+
+add_filter('plugin_action_links', 'wp_ionicons_cheatsheet_action_link', 10, 2);
+function wp_ionicons_cheatsheet_action_link($links, $file) {
+  $settings_link = '<a href="http://ionicons.com/cheatsheet.html" target="_blank">Cheat Sheet</a>';
+  array_unshift($links, $settings_link);
+  return $links;
+}
+
+function wp_ionicons_css() {
+  wp_register_style('ionicons_css', 'https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css', array(), '1.0');
+  wp_enqueue_style('ionicons_css');
+}
+add_action('wp_enqueue_scripts', 'wp_ionicons_css');
+
+function wp_ionicons_shortcode( $atts ) {
+  extract( shortcode_atts( array( 'icon' => 'home', ), $atts ) );
+  return '<i class="icon ion-'.str_replace('ion-', '', $icon).'"></i>';
+}
+
+add_shortcode('icon', 'wp_ionicons_shortcode');
+add_filter('wp_nav_menu_items', 'do_shortcode');
+add_filter('widget_text', 'do_shortcode');
+add_filter('widget_title', 'do_shortcode');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,20 @@
+=== Plugin Name ===
+Contributors: maxlynch
+Tags: ionicons, ionic, hybrid, mobile, font icon, font icons, fontawesome, shortcode, social icons, social media icons, svg icons, menu icons, free icons
+Requires at least: 3.2
+Tested up to: 4.3
+Stable tag: 1.0
+License: MIT
+
+Add over _900_ free and open source mobile, iOS, Android, and social media icons to your wordpress site with the only official [Ionicons](http://ionicons.com/) WordPress plugin.
+
+== Description ==
+
+This plugin adds a simple `[icon]` shortcode for adding icons. For example, add the wonderful earth icon with `[icon name=ion-earth]`. You can
+also use this plugin with HTML like this: `<i class="ion-earth"></i>`
+
+== Installation ==
+
+1. Unzip the download package
+2. Upload `ionicons` to the `/wp-content/plugins/` directory
+3. Activate the plugin through the 'Plugins' menu in WordPress
